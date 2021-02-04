3f2252a1c93934d3d300c7c7cebf27a1c608083f
Kowalski Dark <darkkowalski2012@gmail.com>
2021-02-05 06:10:24 +0800

    Update

--- ext/ruar/extconf.rb
+++ ext/ruar/extconf.rb
@@ -4,6 +4,6 @@ require 'mkmf'
 $CFLAGS << ' -O3 '
 $CFLAGS << ' -std=c99'
 
-return nil unless have_library('z') && have_header('zlib.h') && have_func('crc32', 'zlib.h')
+return unless have_library('z') && have_header('zlib.h') && have_func('crc32', 'zlib.h')
 
 create_makefile('ruar/ruar')
