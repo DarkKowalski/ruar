f57bbe4339c99782639749ce14446a07f2e8c860
Kowalski Dark <darkkowalski2012@gmail.com>
2021-02-05 04:29:49 +0800

    Update

--- test/access_test.rb
+++ test/access_test.rb
@@ -4,12 +4,12 @@ require 'test_helper'
 
 class AccessTest < Minitest::Test
   def test_ruar_can_read_archive_header_using_plain_mode
-    access = Ruar::Access.new('/tmp/plain.ruar')
+    access = Ruar::Access.new('./tmp/plain.ruar')
     puts access.header
   end
 
   def test_ruar_can_read_archive_index_using_plain_mode
-    access = Ruar::Access.new('/tmp/plain.ruar')
+    access = Ruar::Access.new('./tmp/plain.ruar')
     puts access.index
   end
 end
--- test/serialize_test.rb
+++ test/serialize_test.rb
@@ -4,7 +4,7 @@ require 'test_helper'
 
 class SerializeTest < Minitest::Test
   def test_ruar_can_serialize_file_using_plain_mode
-    Ruar::Serialize.plain('./test/sample', '/tmp/plain.ruar')
+    Ruar::Serialize.plain('./test/sample', './tmp/plain.ruar')
     # puts header_hex = `head -c 64 /tmp/plain.ruar | hexdump -C `
   end
 end
