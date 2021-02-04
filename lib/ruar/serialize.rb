b89fdc7665ca6347b035996fdaa0e62d2dca4cab
Kowalski Dark <darkkowalski2012@gmail.com>
2021-02-05 06:01:32 +0800

    Update

--- lib/ruar/serialize.rb
+++ lib/ruar/serialize.rb
@@ -6,7 +6,6 @@ module Ruar
       index = Ruar::Index.new(srcdir)
       Ruar::Serialize::Native.plain_header(dstfile, index.json_index)
       index.source_info.each do |src|
-        puts src['realpath']
         Ruar::Serialize::Native.append_file(dstfile, src['realpath'])
       end
     end
--- test/access_test.rb
+++ test/access_test.rb
@@ -5,12 +5,14 @@ require 'test_helper'
 class AccessTest < Minitest::Test
   def test_ruar_can_read_archive_header_using_plain_mode
     archive = File.join(Dir.tmpdir, 'plain.ruar')
+    Ruar::Serialize.plain('./test/sample', archive)
     access = Ruar::Access.new(archive)
     refute_nil(access.header)
   end
 
   def test_ruar_can_read_archive_index_using_plain_mode
     archive = File.join(Dir.tmpdir, 'plain.ruar')
+    Ruar::Serialize.plain('./test/sample', archive)
     access = Ruar::Access.new(archive)
     refute_nil(access.index)
   end
