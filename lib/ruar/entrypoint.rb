699f9b03d3736984d955c3ba4dd59d642be36a6d
Kowalski Dark <darkkowalski2012@gmail.com>
2021-02-08 03:40:59 +0800

    Update

--- lib/ruar/entrypoint.rb
+++ lib/ruar/entrypoint.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 module Ruar
-  module EntryPoint
+  class EntryPoint
     def self.setup(option = { archive: nil, entry: nil })
-      @@archive = option[:archive]
-      @@entry = option[:entry]
+      @archive = option[:archive]
+      @entry = option[:entry]
       self
     end
 
     def self.activate
-      @@access = Ruar::Access.new(@@archive)
-      @@access.eval(@@entry) unless @@entry.nil?
+      @access = Ruar::Access.new(@archive)
+      @access.eval(@entry) unless @entry.nil?
       self
     end
 
     def self.require(path)
-      @@access.eval(path)
+      @access.eval(path)
     end
   end
 end
--- test/sample/y/err.rb
+++ test/sample/y/err.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
 def err
-  raise "real_file => 'y/err.rb'"
+  raise({
+    'env_file' => __FILE__,
+    'env_lineno' => __LINE__,
+    'real_file' => 'y/err.rb'
+  }.to_s)
 end
